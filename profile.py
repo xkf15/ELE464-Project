e6c8406b10a74dcb0ebce18098b548168995cfd3
xkf15 <13051775153@163.com>
2020-05-11 02:25:18 -0400

    Create method to write profiling data

--- profile.py
+++ profile.py
@@ -1,18 +1,33 @@
 from invoke import invokeAction, invokeWeb
-#from concurrent import futures
-from multiprocessing import Process
+from multiprocessing import Process, Lock
 import time
+import pandas as pd
+import csv
 
-def invokeParallel(index):
-   #res = invoke('helloPython', {'name': 'Kaifeng',})
-   res = invokeWeb('autocomplete/uspresidents', {'term':'Ge'}, {})
-   print(str(index) + res)
+profile_data = pd.DataFrame(columns= ['Function Name', 'Start Time', 'End Time'])
+profile_data.to_csv(r'./profile_data.csv', index = False)
 
-#with futures.ProcessPoolExecutor() as pool:
+def invokeParallel(i, l):
+   name = 'autocomplete/uspresidents'
+   t_start = time.time()
+   res = invokeWeb(name, {'term':'Ge'}, {})
+   t_end = time.time()
+   l.acquire()
+   #profile_data.append({'Function Name': name, 'Start Time': t_start, 'End Time': t_end  }, ignore_index=True)
+   with open(r'profile_data.csv','a') as fd:
+       writer = csv.writer(fd)
+       writer.writerow([name, t_start, t_end])
+   print(str(i) + res + "\tDuration: " + str(t_end - t_start))
+   l.release()
+   if i == 9:
+       print(pd.read_csv('profile_data.csv'))
+
 for i in range(10):
-    #res = pool.submit(invokeParallel, i)
-    Process(target=invokeParallel, args=(i,)).start()
-    time.sleep(0.1)
+    l = Lock()
+
+    t_start = time.time()
+    Process(target=invokeParallel, args=(i,l)).start()
+
 
 
 
